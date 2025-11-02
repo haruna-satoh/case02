50556fd97bc502c7c203c1a3a706d4a145e8b297
haruna <news.sigeak@gmail.com>
2025-11-02 22:27:38 +0900

    ログインページのルーティング

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/admin/login', [AdminController::class, 'adminlogin'])->name('admin.login');
