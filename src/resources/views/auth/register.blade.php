e6d549194dd17b9a7fb91861a5a6e259c9296936
haruna <news.sigeak@gmail.com>
2025-11-04 23:16:25 +0900

    会員登録ページのヘッダー継承

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -1,1 +1,9 @@
-<h1>会員登録</h1>
+@extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
+@endsection
+
+@section('content')
+    <h1>会員登録</h1>
+@endsection
