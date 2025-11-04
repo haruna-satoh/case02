2bdd21a4278056ba23afecbd9a135f3f02c214cb
haruna <news.sigeak@gmail.com>
2025-11-04 22:04:27 +0900

    adminガードの使用設定

--- src/app/Http/Controllers/AdminController.php
+++ src/app/Http/Controllers/AdminController.php
@@ -3,10 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\LoginRequest;
+use App\Models\Admin;
 
 class AdminController extends Controller
 {
     public function adminlogin() {
         return view('auth.login', ['isAdmin' => true]);
+    }
+
+    public function login(LoginRequest $request) {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::guard('admin')->attempt($credentials)){
+            return redirect()->return('admin.index');
+        }
+
+        return back()->withErrors([
+            "login" => "ログイン情報が登録されていません",
+        ])->withInput();
     }
 }
