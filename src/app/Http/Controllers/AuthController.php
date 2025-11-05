d9e68325ad611ddfd11cf1d5e7aa1e2799165aa4
haruna <news.sigeak@gmail.com>
2025-11-06 06:15:39 +0900

    新規登録ページのアクション設定

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -13,5 +15,15 @@ class AuthController extends Controller
 
     public function register() {
         return view('auth.register');
+    }
+
+    public function store(Request $request) {
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect('/attendance');
     }
 }
