2b29ee5fcf5a286946e491343be9c1957ca110df
haruna <news.sigeak@gmail.com>
2025-11-05 11:18:48 +0900

    新規登録ページの名前入力欄作成

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -5,5 +5,28 @@
 @endsection
 
 @section('content')
-    <h1>会員登録</h1>
+    <div class="register__time-cade">
+        <div class="register__title">
+            <h1>会員登録</h1>
+        </div>
+        <form action="/register" method="post" class="form">
+            @csrf
+            <div class="form__group">
+                <div class="form__group--title">
+                    <span>名前</span>
+                </div>
+                <div class="form__group--content">
+                    <div class="form__group--input">
+                        <input type="name" name="name" value="{{ old('name') }}">
+                    </div>
+                    <div class="form__error">
+                        @error('name')
+                            {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+                
+            </div>
+        </form>
+    </div>
 @endsection
