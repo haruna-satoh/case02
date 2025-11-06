aacc2c82105ff27f98beb8018e99cace98d4243b
haruna <news.sigeak@gmail.com>
2025-11-06 23:45:09 +0900

    新規登録ページのバリデーション

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
@@ -17,7 +18,7 @@ class AuthController extends Controller
         return view('auth.register');
     }
 
-    public function store(Request $request) {
+    public function store(RegisterRequest $request) {
         User::create([
             'name' => $request->name,
             'email' => $request->email,
