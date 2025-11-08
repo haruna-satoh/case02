d101d0cca3139804210126d73e7e6f5b5808b068
haruna <news.sigeak@gmail.com>
2025-11-08 23:16:58 +0900

    一般ログインページのアクション設定

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -7,11 +7,24 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login() {
+    public function showLogin() {
         return view('auth.login', ['isAdmin' => false]);
+    }
+
+    public function login(LoginRequest $request) {
+        $credentials = $request->only('email','password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect('/attendance');
+        }
+
+        return back()->withErrors([
+            'login' => 'ログイン情報が登録されていません',
+        ])->withInput();
     }
 
     public function register() {
