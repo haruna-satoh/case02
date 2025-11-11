b649a0fce397e20087ff3f57f11498a7b6933cef
haruna <news.sigeak@gmail.com>
2025-11-11 22:53:58 +0900

    管理者用勤怠一覧ページのタイトル

---
+++ src/resources/views/admin/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/admin/index.css') }}">
+@endsection
+
+@section('content')
+    <div class="attendance">
+        <div class="attendance__header">
+            <h2 class="attendance__title">
+                2023年6月1日の勤怠
+            </h2>
+        </div>
+    </div>
+@endsection
