b95c10b18c7882999d395d2ca43249dcaf8ab00d
haruna <news.sigeak@gmail.com>
2025-11-12 09:54:37 +0900

    テーブルの見出し表示

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -22,5 +22,16 @@
                 </button>
             </div>
         </div>
+
+        <table class="attendance__table">
+            <tr>
+                <th>名前</th>
+                <th>出勤</th>
+                <th>退勤</th>
+                <th>休憩</th>
+                <th>合計</th>
+                <th>詳細</th>
+            </tr>
+        </table>
     </div>
 @endsection
