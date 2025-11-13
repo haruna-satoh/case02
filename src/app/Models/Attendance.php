1c79a2f407b26f58beda22f47deefeb0026df5fa
haruna <news.sigeak@gmail.com>
2025-11-13 10:08:56 +0900

    データベース保存とリレーション設定

---
+++ src/app/Models/Attendance.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\BreakTime;
+
+class Attendance extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'date',
+        'start_time',
+        'end_time',
+        'total_time',
+    ];
+
+    public function breakTimes() {
+        return $this->hasMany(BreakTime::class);
+    }
+}
---
+++ src/app/Models/BreakTime.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BreakTime extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'attendance_id',
+        'start_time',
+        'end_time',
+        'break_time',
+    ];
+
+    public function attendance() {
+        return $this->belongsTo(Attendance::class);
+    }
+}
