feb95aa52f3ce2f815017361dc2adf65e2718de7
haruna <news.sigeak@gmail.com>
2025-11-13 22:41:37 +0900

    シーダーファイル登録

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AdminSeeder::class);
+        $this->call([
+            AdminSeeder::class,
+            UserSeeder::class,
+            AttendanceSeeder::class,
+        ]);
     }
 }
