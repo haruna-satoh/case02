287ad82bc60ebb93b588d620979fe59df7dac487
haruna <news.sigeak@gmail.com>
2025-12-01 00:43:41 +0900

    保存データを取得して表示する設定

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -8,19 +8,22 @@
     <div class="attendance">
         <div class="attendance__header">
             <h2 class="attendance__title">
-                2023年6月1日の勤怠
+                {{ $date }}の勤怠
             </h2>
             <div class="attendance__date-nav--cade">
                 <div class="attendance__date-nav">
-                    <button class="attendance__button">
-                        前日
-                    </button>
-                    <div class="attendance__date-input">
-                        <input type="date" name="date" value="2023-06-01">
-                    </div>
-                    <button class="attendance__button">
-                        翌日
-                    </button>
+                    <form action="{{ route('admin.attendance.index') }}" method="get" class="attendance__date-nav">
+                        @csrf
+                        <button class="attendance__button" type="submit" name="nav" value="{{ \Carbon\Carbon::parse($date)->subDay()->toDateString() }}">
+                            ← 前日
+                        </button>
+                        <div class="attendance__date-input">
+                            <input type="date" name="selected_date" value="{{ $date }}">
+                        </div>
+                        <button class="attendance__button" type="submit" name="nav" value="{{ \Carbon\Carbon::parse($date)->addDay()->toDateString() }}">
+                            翌日 →
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
@@ -35,54 +38,18 @@
                     <th>合計</th>
                     <th>詳細</th>
                 </tr>
-                <tr>
-                    <td>山田太郎</td>
-                    <td>09:00</td>
-                    <td>18:00</td>
-                    <td>1:00</td>
-                    <td>8:00</td>
-                    <td><a href="/admin/attendance/{id}">詳細</a></td>
-                </tr>
-                <tr>
-                    <td>西伶奈</td>
-                    <td>09:00</td>
-                    <td>18:00</td>
-                    <td>1:00</td>
-                    <td>8:00</td>
-                    <td><a href="/admin/attendance/{id}">詳細</a></td>
-                </tr>
-                <tr>
-                    <td>増田一世</td>
-                    <td>09:00</td>
-                    <td>18:00</td>
-                    <td>1:00</td>
-                    <td>8:00</td>
-                    <td><a href="/admin/attendance/{id}">詳細</a></td>
-                </tr>
-                <tr>
-                    <td>山本敬吉</td>
-                    <td>09:00</td>
-                    <td>18:00</td>
-                    <td>1:00</td>
-                    <td>8:00</td>
-                    <td><a href="/admin/attendance/{id}">詳細</a></td>
-                </tr>
-                <tr>
-                    <td>秋田朋美</td>
-                    <td>09:00</td>
-                    <td>18:00</td>
-                    <td>1:00</td>
-                    <td>8:00</td>
-                    <td><a href="/admin/attendance/{id}">詳細</a></td>
-                </tr>
-                <tr>
-                    <td>中西教夫</td>
-                    <td>09:00</td>
-                    <td>18:00</td>
-                    <td>1:00</td>
-                    <td>8:00</td>
-                    <td><a href="/admin/attendance/{id}">詳細</a></td>
-                </tr>
+                @foreach($attendances as $attendance)
+                    <tr>
+                        <td>{{ $attendance->user->name }}</td>
+                        <td>{{ $attendance->start_time }}</td>
+                        <td>{{ $attendance->end_time }}</td>
+                        <td>{{ $attendance->formatted_break_time }}</td>
+                        <td>{{ $attendance->formatted_work_minutes }}</td>
+                        <td>
+                            <a href="{{ route('admin.attendance.show', $attendance->id) }}">詳細</a>
+                        </td>
+                    </tr>
+                @endforeach
             </table>
         </div>
     </div>
