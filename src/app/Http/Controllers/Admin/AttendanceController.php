c843ed9a140402d5516ac1dd1c85a7425372e2c6
haruna <news.sigeak@gmail.com>
2025-12-01 00:12:11 +0900

    管理者用勤怠一覧ページで指定した日付のデータ取得機能

--- src/app/Http/Controllers/Admin/AttendanceController.php
+++ src/app/Http/Controllers/Admin/AttendanceController.php
@@ -8,9 +8,23 @@ use App\Models\Attendance;
 
 class AttendanceController extends Controller
 {
-    public function index() {
-        $attendances = Attendance::with('breakTimes', 'user')->get();
+    public function index(Request $request) {
+        $date = $request->get('date');
 
-        return view('admin.index', compact('attendances'));
+        if (!$date) {
+            $date = Attendance::max('date');
+        }
+
+        if ($request->nav) {
+            $date = $request->nav;
+        }else {
+            $date = $request->selected_date ?? Attendance::max('date');
+        }
+
+        $attendances = Attendance::where('date', $date)
+            ->with(['user', 'breakTimes'])
+            ->get();
+
+        return view('admin.index', compact('attendances', 'date'));
     }
 }
