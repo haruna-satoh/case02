8e4f8bf3af60b1e3e5a2745da92fd3697da0ba1c
haruna <news.sigeak@gmail.com>
2025-12-16 23:02:38 +0900

    出勤機能のアクション

--- src/app/Http/Controllers/AttendanceController.php
+++ src/app/Http/Controllers/AttendanceController.php
@@ -17,4 +17,37 @@ class AttendanceController extends Controller
 
         return view('attendance.index', compact('attendance'));
     }
+
+    public function start() {
+        $user = Auth::user();
+        $today = Carbon::today();
+
+        $attendance = Attendance::firstOrCreate(
+            [
+                'user_id' => $user->id,
+                'date' => $today,
+            ],
+            [
+                'status' => '出勤中',
+                'start_time' => Carbon::now()->format('H:i'),
+            ]
+        );
+
+        if ($attendance->start_time === null) {
+            $attendance->update([
+                'start_time' => Carbon::now()->format('H:i'),
+                'status' => '出勤中',
+            ]);
+        }
+
+        return redirect()->route('attendance.index');
+    }
+
+    public function end() {
+        return redirect()->back();
+    }
+
+    public function breakStart() {
+        return redirect()->back();
+    }
 }
