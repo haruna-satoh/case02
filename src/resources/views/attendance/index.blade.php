9990b9c4e6e703af6ea90fbd42bb887c504ee9dd
haruna <news.sigeak@gmail.com>
2025-12-17 16:48:16 +0900

    クラス分け

--- src/resources/views/attendance/index.blade.php
+++ src/resources/views/attendance/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('css')
-    
+    <link rel="stylesheet" href="{{ asset('css/attendance/index.css') }}">
 @endsection
 
 @section('content')
@@ -9,40 +9,42 @@
         <p class="attendance-status">
             @if (!$attendance)
                 勤務外
-            @elseif ($attendance->status === '出勤中')
-                出勤中
             @elseif ($attendance->status === '休憩中')
                 休憩中
-            @elseif ($attendance->status === '退勤済')
+            @elseif ($attendance && !$attendance->end_time)
+                出勤中
+            @else
                 退勤済
             @endif
         </p>
 
-        <p>{{ now()->format('Y年m月d日') }}</p>
-        <p>{{ now()->format('H:i') }}</p>
+        <p class="attendance-date">{{ now()->isoFormat('Y年M月D日(ddd)') }}</p>
+        <p class="attendance-time">{{ now()->format('H:i') }}</p>
 
-        @if (!$attendance)
-            <form action="{{ route('attendance.start') }}" method="post">
-                @csrf
-                <button type="submit">出勤</button>
-            </form>
-        @elseif ($attendance->status === '休憩中')
-            <form action="{{ route('attendance.break.end') }}" method="post">
-                @csrf
-                <button type="submit">休憩戻</button>
-            </form>
-        @elseif ($attendance && !$attendance->end_time)
-            <form action="{{ route('attendance.end') }}" method="post">
-                @csrf
-                <button type="submit">退勤</button>
-            </form>
+        <div class="attendance-buttons">
+            @if (!$attendance)
+                <form action="{{ route('attendance.start') }}" method="post">
+                    @csrf
+                    <button type="submit" class="btn btn-primary">出勤</button>
+                </form>
+            @elseif ($attendance->status === '休憩中')
+                <form action="{{ route('attendance.break.end') }}" method="post">
+                    @csrf
+                    <button type="submit" class="btn btn-secondary">休憩戻</button>
+                </form>
+            @elseif ($attendance && !$attendance->end_time)
+                <form action="{{ route('attendance.end') }}" method="post">
+                    @csrf
+                    <button type="submit" class="btn btn-primary">退勤</button>
+                </form>
 
-            <form action="{{ route('attendance.break.start') }}" method="post">
-                @csrf
-                <button type="submit">休憩入</button>
-            </form>
-        @else
-            <p>お疲れ様でした。</p>
-        @endif
+                <form action="{{ route('attendance.break.start') }}" method="post">
+                    @csrf
+                    <button type="submit" class="btn btn-secondary">休憩入</button>
+                </form>
+            @else
+                <p class="attendance-finish">お疲れ様でした。</p>
+            @endif
+        </div>
     </div>
 @endsection
