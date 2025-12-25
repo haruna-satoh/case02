b45a4b23415cb8c86710f73fee2cba5c16093500
haruna <news.sigeak@gmail.com>
2025-12-25 10:30:32 +0900

    管理者用申請一覧ページのタイトルとテーブル見出し表示

---
+++ src/resources/views/admin/attendance/change/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2>申請一覧</h2>
+
+    <table>
+        <thead>
+            <tr>
+                <th>状態</th>
+                <th>名前</th>
+                <th>対象日時</th>
+                <th>申請理由</th>
+                <th>申請日時</th>
+                <th>詳細</th>
+            </tr>
+        </thead>
+
+        
+    </table>
+@endsection
