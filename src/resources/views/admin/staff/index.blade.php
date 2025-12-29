1e7e4a7a2fb1636c2606a02d11cf6132e15b6e64
haruna <news.sigeak@gmail.com>
2025-12-29 22:40:29 +0900

    スタッフ一覧ページのレイアウト

---
+++ src/resources/views/admin/staff/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/admin/staff/index.css') }}">
+@endsection
+
+@section('content')
+    <h2>スタッフ一覧</h2>
+
+    <table>
+        <thead>
+            <tr>
+                <th>名前</th>
+                <th>メールアドレス</th>
+                <th>月次勤怠</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+                <tr>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>
+                        <a href="#">詳細</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
