c8fb0c41ccf2bff7c85164c5eeefba1152aa2e8a
haruna <news.sigeak@gmail.com>
2025-12-30 17:24:24 +0900

    スタッフ別勤怠一覧ページのアクション

--- src/app/Http/Controllers/Admin/StaffController.php
+++ src/app/Http/Controllers/Admin/StaffController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Attendance;
 
 class StaffController extends Controller
 {
@@ -12,5 +13,13 @@ class StaffController extends Controller
         $users = User::orderBy('id')->get();
 
         return view('admin.staff.index', compact('users'));
+    }
+
+    public function show($userId) {
+        $user = User::findOrFail($userId);
+
+        $attendances = Attendance::where('user_id', $user->id)->orderBy('date', 'desc')->get();
+
+        return view('admin.staff.show', compact('user', 'attendances'));
     }
 }
