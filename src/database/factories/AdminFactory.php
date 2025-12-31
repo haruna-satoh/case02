d4486cce8c3c63257b30f9fe6d8f0c12f69a6699
haruna <news.sigeak@gmail.com>
2025-12-31 16:01:31 +0900

    管理者用Factory作成

---
+++ src/database/factories/AdminFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Admin;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class AdminFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    protected $model = Admin::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => bcrypt('password'),
+        ];
+    }
+}
