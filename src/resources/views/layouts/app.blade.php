538846c093c786e3891ec4063294e136a894e10f
haruna <news.sigeak@gmail.com>
2026-01-01 15:39:26 +0900

    ヘッダーのレイアウト

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -18,6 +18,48 @@
                     <img src="{{ asset('images/logo.svg') }}" alt="COACHTECHロゴ">
                 </a>
             </div>
+
+            @if(Auth::guard('admin')->check())
+                <nav class="header__nav">
+                    <ul class="header__list">
+                        <li>
+                            <a href="/admin/attendance/list">勤怠一覧</a>
+                        </li>
+                        <li>
+                            <a href="/admin/staff/list">スタッフ一覧</a>
+                        </li>
+                        <li>
+                            <a href="/stamp_correction_request/list">申請一覧</a>
+                        </li>
+                        <li>
+                            <form action="/admin/logout" method="post">
+                                @csrf
+                                <button type="submit">ログアウト</button>
+                            </form>
+                        </li>
+                    </ul>
+                </nav>
+            @elseif(Auth::check())
+                <nav class="header__nav">
+                    <ul class="header__list">
+                        <li>
+                            <a href="/attendance">勤怠</a>
+                        </li>
+                        <li>
+                            <a href="/attendance/list">勤怠一覧</a>
+                        </li>
+                        <li>
+                            <a href="/stamp_correction_request/list">申請</a>
+                        </li>
+                        <li>
+                            <form action="/logout" method="post">
+                                @csrf
+                                <button type="submit">ログアウト</button>
+                            </form>
+                        </li>
+                    </ul>
+                </nav>
+            @endif
         </div>
     </header>
 
