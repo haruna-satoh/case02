71c7650a7f6956a4dfb363681f058cd66870046c
haruna <news.sigeak@gmail.com>
2026-01-02 20:57:05 +0900

    AttendanceFactory作成

---
+++ src/database/factories/AttendanceFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Attendance;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Carbon\Carbon;
+
+class AttendanceFactory extends Factory
+{
+    protected $model = Attendance::class;
+
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'date' => Carbon::today(),
+            'start_time' => now(),
+            'end_time' => null,
+        ];
+    }
+}
