f0242291057946203d31cb399b3f1e14c4c671e4
haruna <news.sigeak@gmail.com>
2026-01-06 23:56:38 +0900

    ログイン前のヘッダー作成

---
+++ src/resources/views/layouts/guest.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Time Cade</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    @yield('css')
+</head>
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <div class="header__logo">
+                <a href="/">
+                    <img src="{{ asset('images/logo.svg') }}" alt="COACHTECHロゴ">
+                </a>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
